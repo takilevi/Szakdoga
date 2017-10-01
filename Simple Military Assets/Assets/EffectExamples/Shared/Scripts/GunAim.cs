463ad766122f49f4afcb99644afd6cee302673f9
Levente Takács <levi.takacs@gmail.com>
2017-10-01 22:12:37 +0200

    2017.10.01
    tutorial scene, start scene

---
+++ Simple Military Assets/Assets/EffectExamples/Shared/Scripts/GunAim.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunAim:MonoBehaviour
+{
+	public int borderLeft;
+	public int borderRight;
+	public int borderTop;
+	public int borderBottom;
+    public float mouseX;
+    public float mouseY;
+
+	private Camera parentCamera;
+	private bool isOutOfBounds;
+
+	void Start () 
+	{
+		parentCamera = GetComponentInParent<Camera>();
+	}
+
+	void Update()
+	{
+		//mouseX = Input.mousePosition.x;
+		//mouseY = Input.mousePosition.y;
+        //Debug.Log(Input.mousePosition);
+
+		if (mouseX <= borderLeft || mouseX >= Screen.width - borderRight || mouseY <= borderBottom || mouseY >= Screen.height - borderTop) 
+		{
+			isOutOfBounds = true;
+		} 
+		else 
+		{
+			isOutOfBounds = false;
+		}
+
+		if (!isOutOfBounds)
+		{
+			transform.LookAt(parentCamera.ScreenToWorldPoint (new Vector3(mouseX, mouseY, 5.0f)));
+		}
+	}
+
+	public bool GetIsOutOfBounds()
+	{
+		return isOutOfBounds;
+	}
+}
+
---
+++ Simple Military Assets/Assets/EffectExamples/Shared/Scripts/JokeFromWebHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class JokeFromWebHelper : MonoBehaviour {
+
+    void Start()
+    {
+        string url = "http://api.icndb.com/jokes/random";
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+    }
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        var N = JSON.Parse(www.text);
+
+        var typeString = N["type"].Value;
+        var jokeId = N["value"]["id"].AsInt;
+        var jokeString = N["value"]["joke"].Value;
+
+        //C#
+        string val = N["data"]["sampleArray"][0];      // val contains "string value"
+
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: \r\n" + " type: " +typeString+ " the joke id: "+ jokeId);
+            Debug.Log("The string: " + jokeString);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+}
